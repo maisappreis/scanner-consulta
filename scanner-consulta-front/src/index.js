2cc25730152c0afcd4da0581fcbe6ff55e85f5fa
Maisa Preis <maisapierinipreis@gmail.com>
2023-04-06 19:48:17 -0300

    Testing..

--- scanner-consulta-front/src/index.js
+++ scanner-consulta-front/src/index.js
@@ -15,6 +15,7 @@ ReactDOM.render(
 // Learn more about service workers: https://cra.link/PWA
 serviceWorker.register();
 
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
